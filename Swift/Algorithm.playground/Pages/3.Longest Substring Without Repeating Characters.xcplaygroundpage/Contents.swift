0a1dbe3e3c1211a0d6ce326aa9b475a408027447
Kyle <ych.wang@outlook.com>
2019-03-28 22:11:24 +0800

    finished 3

--- Swift/Algorithm.playground/Pages/3.Longest Substring Without Repeating Characters.xcplaygroundpage/Contents.swift
+++ Swift/Algorithm.playground/Pages/3.Longest Substring Without Repeating Characters.xcplaygroundpage/Contents.swift
@@ -5,37 +5,24 @@ import Foundation
 class Solution {
     
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        // 使用 2个 character的数组, 一个为之前最长的数组 一个为当前数组
-        
-        // 遍历输入字符串的所有 字符
-        // > 检查当前的字符, 若当前数组包含此字符, 就从重复的位置截断当前数组, 在拼接此字符,未重复就添加到当前数组,
-        // > 判断当前数组与之前最长数组, 赋值
-        // >        
-        var arrLongest = [Character](), currentArr = arrLongest
-        for (i,c) in s.enumerated() {
-            if let index = currentArr.firstIndex(of: c) {
-                if index == currentArr.count - 1 {
-                    currentArr = [c]
-                } else {
-                    currentArr.removeSubrange(0...index)
-                    currentArr.append(c)
-                }
-            } else {
-                currentArr.append(c)
+        var map = [Character: Int](), i = 0, j = 0, longest = 0
+        let sChars = Array(s)
+        while j < sChars.count {
+            let c = sChars[j]
+            if let index = map[c] {
+                i = max(i, index)
             }
-            if currentArr.count > arrLongest.count {
-                arrLongest = currentArr
-            }
-            if arrLongest.count - currentArr.count == s.count - i - 1 {
-                break
-            }
+            // 1.
+            longest = max(longest, j - i + 1)
+            j += 1
+            // 在j+=1之后在存到map中, 以及在1.处计算的 + 1, 是为了兼容 string长度为1的字符串
+            map[c] = j
         }
-//        print(arrLongest)
-        return arrLongest.count
+        return longest
     }
     
 }
 
-print(Solution().lengthOfLongestSubstring("pwwkewfsa;lkdjfalkfdjaljfdlaksfkahfajkdhsflajksdhfasdfadsf,pwwkewfsa;lkdjfalkfdjaljfdlaksfkahfajkdhsflajksdhfasdfadsf,pwwkewfsa;lkdjfalkfdjaljfdlaksfkahfajkdhsflajksdhfasdfadsf,pwwkewfsa;lkdjfalkfdjaljfdlaksfkahfajkdhsflajksdhfasdfadsf,pwwkewfsa;lkdjfalkfdjaljfdlaksfkahfajkdhsflajksdhfasdfadsf,pwwkewfsa;lkdjfalkfdjaljfdlaksfkahfajkdhsflajksdhfasdfadsf,pwwkewfsa;lkdjfalkfdjaljfdlaksfkahfajkdhsflajksdhfasdfadsf"))
+print(Solution().lengthOfLongestSubstring("pwwkewabc"))
 
 //: [Next](@next)
